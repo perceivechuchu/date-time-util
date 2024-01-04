42600cf407744aa0244c580e89296aec4dff4ffb
Perceive Chuchu <perceive.chuchu@momentum.co.za>
2024-01-04 09:40:15 +0200

    rename package structure

---
+++ src/main/java/io/github/perceivechuchu/datetimeutil/commons/model/ValidationEntry.java
@@ -0,0 +1,46 @@
+package io.github.perceivechuchu.datetimeutil.commons.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * ValidationEntry class holds the value and message during parameter validation
+ *
+ * @since 1.0.0
+ *
+ * @author Perceive Chuchu
+ */
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationEntry {
+
+    /**
+     * The value to validate
+     *
+     * @since 1.0.0
+     */
+    private Object value;
+
+    /**
+     * The message to respond with when validation fails
+     *
+     * @since 1.0.0
+     */
+    private String message;
+
+    /**
+     * Instantiates new ValidationEntry object
+     *
+     * @param value the value to validate
+     * @param message message to respond with when validation fails
+     * @return the new ValidationEntry object
+     * @since 1.0.0
+     */
+    public static ValidationEntry getInstance(Object value, String message) {
+        return new ValidationEntry(value, message);
+    }
+}
---
+++ src/test/java/io/github/perceivechuchu/datetimeutil/timezone/TimeZoneUtilTests.java
@@ -0,0 +1,75 @@
+package io.github.perceivechuchu.datetimeutil.timezone;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import io.github.perceivechuchu.datetimeutil.constant.ErrorMessages;
+import io.github.perceivechuchu.datetimeutil.model.TimeZoneDetail;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Perceive Chuchu
+ */
+@ExtendWith(MockitoExtension.class)
+class TimeZoneUtilTests {
+
+    @Test
+    void getAllTimeZones_ReturnCorrectTimeZoneDetailsList_WhenGetAllTimeZonesIsInvoked() {
+        List<TimeZoneDetail> timeZones = TimeZoneUtil.getAllTimeZones();
+        assertNotNull(timeZones);
+        // Verify that Time Zone with Zone Id (Africa/Johannesburg) exists in the list
+        TimeZoneDetail timeZone = timeZones.stream().filter(timeZoneDetail -> timeZoneDetail.getId().equals("Africa/Johannesburg")).collect(Collectors.toList()).get(0);
+        System.out.println(timeZone);
+        assertEquals(7200000, timeZone.getOffsetMillis());
+        assertEquals("+02:00", timeZone.getOffsetText());
+        assertEquals("SAST", timeZone.getAbbreviation());
+        assertEquals("South Africa Standard Time", timeZone.getDisplayName());
+    }
+
+    @Test
+    void isValidTimeZoneId_ReturnTrue_WhenTimeZoneIdIsValid() {
+        assertTrue(TimeZoneUtil.isValidTimeZoneId("Africa/Johannesburg"));
+    }
+
+    @Test
+    void isValidTimeZoneId_ReturnFalse_WhenTimeZoneIdIsInvalid() {
+        assertFalse(TimeZoneUtil.isValidTimeZoneId("invalid_timezone_id"));
+    }
+
+    @Test
+    void isValidTimeZoneId_ReturnTrue_WhenTimeZoneIdIsGMT() {
+        assertTrue(TimeZoneUtil.isValidTimeZoneId("GMT"));
+    }
+
+    @Test
+    void getTimeZoneAbbreviation_ReturnTimeZoneAbbreviation_WhenValidTimeZoneValueIsReceived() {
+        assertEquals("SAST", TimeZoneUtil.getTimeZoneAbbreviation("Africa/Johannesburg"));
+    }
+
+    @Test
+    void etTimeZoneAbbreviation_ThrowIllegalArgumentException_WhenLocalDateTimeTextIsNull() {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> TimeZoneUtil.getTimeZoneAbbreviation(null));
+        assertNotNull(illegalArgumentException);
+        assertEquals(ErrorMessages.TIME_ZONE_ID_EMPTY, illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void getTimeZoneAbbreviation_ThrowIllegalArgumentException_WhenLocalDateTimeTextIsInvalid() {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> TimeZoneUtil.getTimeZoneAbbreviation("invalid_timezone_id"));
+        assertNotNull(illegalArgumentException);
+        assertEquals(ErrorMessages.TIME_ZONE_ID_INVALID, illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void searchByOffsetText_ReturnCorrectTimeZoneDetailsList_WhenValidOffsetTextIsReceived() {
+        List<TimeZoneDetail> timeZoneDetails = TimeZoneUtil.searchTimeZonesByOffsetText("+02:00");
+        assertFalse(timeZoneDetails.isEmpty());
+        List<String> abbreviationList = timeZoneDetails.stream().map(TimeZoneDetail::getAbbreviation).collect(Collectors.toList());
+        assertTrue(abbreviationList.contains("SAST"));
+    }
+
+}
